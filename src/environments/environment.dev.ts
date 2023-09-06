0a4dbed7543347d80e493aaedb204ee8148f943b
Bryan Parada <bryan.parada.cea@gmail.com>
2023-09-06 11:59:25 -0300

    E44 - Firebase Configuration

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -1,4 +1,15 @@
 export const environment = {
   production: false,
-  name:'dev'
+  name:'dev',
+  firebase: {
+    config : {
+      apiKey: "AIzaSyDFuJCLOaNphDjLidu8lPuISQ9oii2oJ74",
+      authDomain: "mapboxapp-4ddac.firebaseapp.com",
+      projectId: "mapboxapp-4ddac",
+      storageBucket: "mapboxapp-4ddac.appspot.com",
+      messagingSenderId: "53674440819",
+      appId: "1:53674440819:web:3b426b7009b2a7deae9206",
+      measurementId: "G-JX8WXMHHWY"
+    }
+  }
 };
